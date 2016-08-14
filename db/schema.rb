e9d4495ced4df467aacbcb5402e3f136d61a2c4c
Eugene Park <parkeugene7676@gmail.com>
2016-08-14 21:38:54 +0900

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806152637) do
+ActiveRecord::Schema.define(version: 20160806162408) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "img"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20160806152637) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -54,6 +53,10 @@ ActiveRecord::Schema.define(version: 20160806152637) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

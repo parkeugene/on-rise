881118f6b17531d836b008be48505916fe3a447d
Eugene Park <parkeugene7676@gmail.com>
2016-08-20 14:40:14 +0900

    Add scaffold projects

--- db/migrate/20160807021925_add_description_to_projects.rb
+++
@@ -1,8 +0,0 @@
-class AddDescriptionToProjects < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :projects, :description, :text
-  	add_column :projects, :subtitle, :string
-  	add_column :projects, :contributor, :string
-  	add_column :projects, :contributor_url, :string
-  end
-end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @project = projects(:one)
+  end
+
+  test "should get index" do
+    get projects_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_project_url
+    assert_response :success
+  end
+
+  test "should create project" do
+    assert_difference('Project.count') do
+      post projects_url, params: { project: { contributor: @project.contributor, contributor_url: @project.contributor_url, description: @project.description, original_url: @project.original_url, subtitle: @project.subtitle, title: @project.title, translation_url: @project.translation_url } }
+    end
+
+    assert_redirected_to project_url(Project.last)
+  end
+
+  test "should show project" do
+    get project_url(@project)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_project_url(@project)
+    assert_response :success
+  end
+
+  test "should update project" do
+    patch project_url(@project), params: { project: { contributor: @project.contributor, contributor_url: @project.contributor_url, description: @project.description, original_url: @project.original_url, subtitle: @project.subtitle, title: @project.title, translation_url: @project.translation_url } }
+    assert_redirected_to project_url(@project)
+  end
+
+  test "should destroy project" do
+    assert_difference('Project.count', -1) do
+      delete project_url(@project)
+    end
+
+    assert_redirected_to projects_url
+  end
 end
